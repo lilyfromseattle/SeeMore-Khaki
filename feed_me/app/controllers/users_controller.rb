37f12c88f74e82cda8482c5f3739b545d7959d63
Katie <kab761@gmail.com>
2014-11-17 16:50:14 -0800

    made user create method

--- feed_me/app/controllers/users_controller.rb
+++ feed_me/app/controllers/users_controller.rb
@@ -1,6 +1,16 @@
 class UsersController < ApplicationController
 
   def create
-  #  raise
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to "/"
+    else
+      render "auth/developer"
+    end
   end
+
+  private
+    def user_params
+      params.permit(:name, :email)
+    end
 end
