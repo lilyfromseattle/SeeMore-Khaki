49d4414af514bdaf0cf62e30e944e8a136ba705a
Linnea <lkdamer@gmail.com>
2014-11-18 13:21:42 -0800

    added associations into users, authors, and authors_users models

--- feed_me/app/models/authors_users.rb
+++ feed_me/app/models/authors_users.rb
@@ -1,2 +1,4 @@
 class AuthorsUsers < ActiveRecord::Base
+  belongs_to :author
+  belongs_to :user
 end
--- feed_me/app/models/user.rb
+++ feed_me/app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  has_many :authors_users
+  has_many :authors, through: :authors_users
 end
