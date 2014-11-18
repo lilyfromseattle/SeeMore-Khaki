d45d09c9c39a0a9c518e44f77b1b3c035ffbcca9
Katie <kab761@gmail.com>
2014-11-18 13:31:19 -0800

    ran new migrations

--- feed_me/db/schema.rb
+++ feed_me/db/schema.rb
@@ -16,6 +16,20 @@ ActiveRecord::Schema.define(version: 20141118204732) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "authors", force: true do |t|
+    t.string   "service"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "authors_users", force: true do |t|
+    t.integer  "author_id"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "twitters", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
