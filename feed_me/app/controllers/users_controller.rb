8663fe3e5340098f5f63c62b4450e22949180c49
Katie <kab761@gmail.com>
2014-11-17 21:17:11 -0800

    implemented signup with omniauth developer strategy (maybe?)

--- feed_me/app/controllers/users_controller.rb
+++ feed_me/app/controllers/users_controller.rb
@@ -1,8 +1,11 @@
 class UsersController < ApplicationController
+  skip_before_filter :verify_authenticity_token, only: :create
+  # otherwise rails clobbers the session because callback is sent as a post request
 
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to "/"
     else
       render "auth/developer"
