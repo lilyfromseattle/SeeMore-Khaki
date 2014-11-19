59d7f2bff8b57544f4751a1aacfcaf36df66ca9e
Katie <kab761@gmail.com>
2014-11-18 16:21:50 -0800

    added flash notice for sign out

--- feed_me/app/controllers/sessions_controller.rb
+++ feed_me/app/controllers/sessions_controller.rb
@@ -8,22 +8,24 @@ skip_before_filter :verify_authenticity_token, only: :create
   def create
     auth_hash = request.env['omniauth.auth']
     form_hash = auth_hash[:info]
-    # raise auth_hash.inspect
+
     current_user = User.find_by_provider(auth_hash[:provider], auth_hash[:uid])
-    puts "*"*80, current_user
     if current_user
       session[:current_user] = current_user.id
-      flash.notice = "Welcome back, #{current_user.name}!"
+      flash.notice = "Welcome back to FeedMe, #{current_user.name}!"
       redirect_to root_path
+
     else
       user = User.new(name: form_hash[:name],
                email: form_hash[:email],
                provider: auth_hash[:provider],
                uid: auth_hash[:uid])
+
       if user.save
         session[:current_user] = user.id
         flash.notice = "Thanks for making a FeedMe account, #{user.name}!"
         redirect_to root_path
+
       else
         render "/auth/:provider"
       end
@@ -31,7 +33,9 @@ skip_before_filter :verify_authenticity_token, only: :create
   end
 
   def destroy
+    user = User.find(session[:current_user])
     session[:current_user] = nil
+    flash.notice = "Peace out, #{user.name}!"
     redirect_to root_path
   end
 
