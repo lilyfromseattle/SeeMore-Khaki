2ead2eb9186a30716309e7ccc531535ccd4b13e0
Katie <kab761@gmail.com>
2014-11-18 17:00:25 -0800

    deleted redundancy

--- feed_me/config/routes.rb
+++ feed_me/config/routes.rb
@@ -7,8 +7,6 @@ Rails.application.routes.draw do
   post    "/auth/:provider/callback", to: "sessions#create",  as: :sessions
   delete  "/users/signout",           to: "sessions#destroy", as: :signout
 
-  post  "/auth/:provider/callback",    to: "users#create", as:   :user_create
-
   get "/users/:id/feed",                        to: "feed#show",        as: :feed
 
   # The priority is based upon order of creation: first created -> highest priority.
