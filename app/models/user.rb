74578310e358c747f91b9446e3c761fe052048b4
Katie <kab761@gmail.com>
2014-11-19 12:28:59 -0800

    wrote passing specs for User.find_by_provider

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ActiveRecord::Base
   # has_many :authors, through: :authors_users
   has_and_belongs_to_many :authors
 
-  # validates :email, presence: true
-  # validates :name, presence: true
-  # validates :uid, presence: true
-  # validates :provider, presence: true
+  validates :email, presence: true
+  validates :name, presence: true
+  validates :uid, presence: true
+  validates :provider, presence: true
 
   def self.find_by_provider(provider, uid)
     self.find_by(provider: provider, uid: uid)
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
-=begin
+
 describe User do
 let(:user) { User.new(
     email:    "a@b.com",
@@ -12,6 +12,7 @@ let(:user) { User.new(
     it "is valid" do
       expect(user).to be_valid
     end
+
     it "requires an email" do
       user.email = nil
       expect(user).to be_invalid
@@ -31,36 +32,34 @@ let(:user) { User.new(
       user.provider = nil
       expect(user).to be_invalid
     end
+
   end
 
   describe "self.find_by_provider" do
 
     it "finds user by provider and uid" do
-      result = user
-      expect(User.find_by_provider(user[:protocol], user[:uid])).to eq result
+      user.save
+      expect(User.find_by_provider(user[:provider], user[:uid]).email).to eq "a@b.com"
     end
 
     it "returns nil for non-existent (new) user" do
+      user.save
       expect(User.find_by_provider("twitter", user[:uid])).to eq nil
     end
   end
 
-
-
-  describe ".initialize_from_omniauth" do
-    let(:user) { User.find_or_create_from_omniauth(OmniAuth.config.mock_auth[:twitter]) }
-
-    it "creates a valid user" do
-      expect(user).to be_valid
-    end
-
-    context "when it's invalid" do
-      it "returns nil" do
-        user = User.find_or_create_from_omniauth({"uid" => "123", "info" => {}})
-        expect(user).to be_nil
-      end
-    end
-  end
+  # describe ".initialize_from_omniauth" do
+  #   let(:user) { User.find_or_create_from_omniauth(OmniAuth.config.mock_auth[:twitter]) }
+  #
+  #   it "creates a valid user" do
+  #     expect(user).to be_valid
+  #   end
+  #
+  #   context "when it's invalid" do
+  #     it "returns nil" do
+  #       user = User.find_or_create_from_omniauth({"uid" => "123", "info" => {}})
+  #       expect(user).to be_nil
+  #     end
+  #   end
+  # end
 end
-
-=end
