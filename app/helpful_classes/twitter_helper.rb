1ad908dcbc80e63838526bd9a606b738cf7078fa
Holly Leary <schleary@gmail.com>
2014-11-21 14:37:19 -0800

    pulling

--- app/helpful_classes/twitter_helper.rb
+++ app/helpful_classes/twitter_helper.rb
@@ -29,12 +29,6 @@ class TwitterHelper
 
   def parse_api
     puts "*****SIX*********************"
-    @client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = ENV["TWITTER_API_KEY"]
-      config.consumer_secret     = ENV["TWITTER_API_SECRET"]
-      config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
-      config.access_token_secret = ENV["TWITTER_TOKEN_SECRET"]
-    end
 
     @api_data = client.user_timeline(@author).take(20)
     puts @api_data.inspect
