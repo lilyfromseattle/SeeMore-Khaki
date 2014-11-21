7e0340fb6056656dc23a757ba4f586fb1901a1b6
Holly Leary <schleary@gmail.com>
2014-11-21 14:29:22 -0800

    still working on twitter feed

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -18,6 +18,7 @@ class FeedController < ApplicationController
         z = TwitterHelper.new(author)
         puts "ARE WE QUERYING FOR POSTS?"
         z.query_for_tweets
+        puts "ANSWERS THE QUESTION"
         @posts += z.tweets
       end
     end
--- app/helpful_classes/twitter_helper.rb
+++ app/helpful_classes/twitter_helper.rb
@@ -1,6 +1,6 @@
 require 'httparty'
 class TwitterHelper
-  attr_accessor :author, :tweets, :client, :search_results
+  attr_accessor :author, :tweets, :client, :search_results, :api_data
   def initialize author
     @author = author
     @api_data = []
@@ -29,33 +29,41 @@ class TwitterHelper
 
   def parse_api
     puts "*****SIX*********************"
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["TWITTER_API_KEY"]
+      config.consumer_secret     = ENV["TWITTER_API_SECRET"]
+      config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+      config.access_token_secret = ENV["TWITTER_TOKEN_SECRET"]
+    end
 
+    @api_data = client.user_timeline(@author).take(20)
+    puts @api_data.inspect
 
     @api_data.each_with_index do |tweet, i|
       @tweets << []
+      puts "TWEET: #{tweet}*********"
       # avatar = tweet.profile_image_url
       @tweets[i] << author = tweet.user
+      puts "USER: #{tweet.user}*********"
       @tweets[i] << text = tweet.text
+      puts "TEXT: #{tweet.text}************"
       @tweets[i] << timestamp = tweet.created_at
       # image = tweet.user.image_path
 
     end
+    puts "***********SEVEN*************"
   end
 
   def query_for_author
-    puts "*******ONE******"
     db_or_api
     # The above method searches the db for the author,
     # does api query if author isn't in db
     if @author.class == Author
-      puts "*****WRONG OPTION*********************"
       @author
     else
-      puts "*****THREE*********************"
       new_author = Author.new(name: @author, service: "Twitter")
       new_author.save
       @author = new_author
-      puts "*****FOUR*********************"
     # else
     #   puts "*****FOURTH OPTION*********************"
 
@@ -63,13 +71,9 @@ class TwitterHelper
   end
 
   def db_or_api
-    puts "*******TWO*********"
-
     if Author.find_by(name: @author, service: "Twitter")
       @author = Author.find_by(name: @author, service: "Twitter")
-      puts "THe database stuff happened"
     else
-      puts "*******THREE***********"
       @client = Twitter::REST::Client.new do |config|
         config.consumer_key        = ENV["TWITTER_API_KEY"]
         config.consumer_secret     = ENV["TWITTER_API_SECRET"]
@@ -80,15 +84,6 @@ class TwitterHelper
       @search_results = client.user_search(@author).take(20)
 
       @api_data = client.user_timeline(@author).take(20)
-      # @api_data.each_with_index do |tweet, i|
-      #   @tweets << []
-      #   # avatar = tweet.profile_image_url
-      #   @tweets[i] << author = tweet.user
-      #   @tweets[i] << text = tweet.text
-      #   @tweets[i] << timestamp = tweet.created_at
-      #   # image = tweet.user.image_path
-      #
-      # end
 
       @api_data.each_with_index do |tweet, i|
         @tweets << []
@@ -99,14 +94,8 @@ class TwitterHelper
         # image = tweet.user.image_path
 
       end
+      # .hashtags.each.text => [#<Twitter::Entity::Hashtag:0x007fea752a9420 @attrs={:text=>"WHD2013", :indices=>[17, 25]}>, #<Twitter::Entity::Hashtag:0x007fea752a93a8 @attrs={:text=>"EveryMileMatters", :indices=>[108, 125]}>, #<Twitter::Entity::Hashtag:0x007fea752a91c8 @attrs={:text=>"BeyGood", :indices=>[126, 134]}>]
 
-        # .hashtags.each.text => [#<Twitter::Entity::Hashtag:0x007fea752a9420 @attrs={:text=>"WHD2013", :indices=>[17, 25]}>, #<Twitter::Entity::Hashtag:0x007fea752a93a8 @attrs={:text=>"EveryMileMatters", :indices=>[108, 125]}>, #<Twitter::Entity::Hashtag:0x007fea752a91c8 @attrs={:text=>"BeyGood", :indices=>[126, 134]}>]
-
-
-
-
-      puts "**tweets: ****#{@tweets.each do |tweet| puts tweet end}*************"
-      # @api_data.
     end
 
 
