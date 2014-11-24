86e290643af64ec27d83843fe72aefd3450c2215
Holly Leary <schleary@gmail.com>
2014-11-24 14:28:55 -0800

    pulling

--- app/helpful_classes/twitter_helper.rb
+++ app/helpful_classes/twitter_helper.rb
@@ -16,8 +16,8 @@ class TwitterHelper
       config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
       config.access_token_secret = ENV["TWITTER_TOKEN_SECRET"]
     end
-
     @api_data = @client.user_timeline(@author.name).take(5)
+    # @author.avatar = @api_data.profile_image_url
     @api_data.each_with_index do |tweet, i|
       @tweets << {
       service: "Twitter",
@@ -31,13 +31,13 @@ class TwitterHelper
   def query_for_author
     db_or_api
     if @author.class == Author
-      @author
       @client = Twitter::REST::Client.new do |config|
         config.consumer_key        = ENV["TWITTER_API_KEY"]
         config.consumer_secret     = ENV["TWITTER_API_SECRET"]
         config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
         config.access_token_secret = ENV["TWITTER_TOKEN_SECRET"]
       end
+      @author.avatar =
       @search_results = @client.user_search(@author.name).take(10)
     else
       new_author = Author.new(name: @author, service: "Twitter")
