2fd88508f3d52a2492b6c9a4c777b3228e873d93
Holly Leary <schleary@gmail.com>
2014-11-24 11:07:06 -0800

    won't create new subscriptions to already-existing twitter subscriptions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,16 +3,26 @@ class UsersController < ApplicationController
   def subscribe
     @user = User.find(session[:current_user])
     @author = Author.find(params[:id])
-    @authors_user = AuthorsUser.new
-  
-    @authors_user.author_id = @author.id
-    @authors_user.user_id = session[:current_user]
-    # redirect_to "/users/#{session[:current_user]}/feed"
-    if @authors_user.save
-
-      flash[:notice] = "You are now subscribed to #{@author.name}!"
+    #if the @authors_user already exists, don't create a new one; give a message
+    puts "DOES THE AUTHORSUSER ALREADY EXIST?: #{AuthorsUser.where(:author_id => @author.id).inspect} **"
+    if AuthorsUser.where(:author_id => @author.id) != []
+      flash[:notice] = "You are already subscribed to #{@author.name}!"
       # redirect_to "/home/subscribed"
       redirect_to "/users/#{session[:current_user]}/feed"
+
+    else
+      puts "CREATING A NEW AU (CORRECT)"
+      @authors_user = AuthorsUser.new
+
+      @authors_user.author_id = @author.id
+      @authors_user.user_id = session[:current_user]
+      # redirect_to "/users/#{session[:current_user]}/feed"
+      if @authors_user.save
+
+        flash[:notice] = "You are now subscribed to #{@author.name}!"
+        # redirect_to "/home/subscribed"
+        redirect_to "/users/#{session[:current_user]}/feed"
+      end
     end
 
   end
